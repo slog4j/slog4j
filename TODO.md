d26ec6fdc3202123bd9fc2ff836ad3a11c69d091
Eliezio Oliveira <eliezio@protonmail.com>
2017-10-16 03:02:22 -0200

    hybrid TextFormatter is now the default

--- TODO.md
+++ TODO.md
@@ -1,20 +1,20 @@
 ## Design
 
 - [x] Option to include timestamp/level on formatted message
-- [ ] How to log stacktraces? Use JSON? Is there any alternative format to the standard multi-line format?
+- [x] How to log stacktraces? Use JSON? Is there any alternative format to the standard multi-line format?
 - [ ] JSONFormatter
 - [ ] Support aggregators other than LogStash
 
 ## Implementation
 
-- [ ] Clean-up every key
+- [x] Clean-up every key
 
 ## Documentation
 
 - [ ] Document API
 - [ ] Java 1.6
 - [ ] cites TechRadar / J. Turnbull
-- [ ] spanId/traceId recommendations
+- [ ] spanId/traceId recommendations; type LongId
 - [ ] Use a conventional domain for specs and examples
 - [ ] Hints on how to keep the `spanId` context-wide available (common cases: Spring Web);
 - [ ] How to use with Spring Sleuth;
@@ -23,4 +23,5 @@
 
 - [x] Configure Travis-CI
 - [ ] Configure publishing to jcenter
+- [ ] Configure [Sonarqube](https://about.sonarcloud.io/news/2016/05/02/continuous-analysis-for-oss-projects.htm) 
 - [ ] Use YourKit to profile
--- src/main/java/org/slog4j/format/TextFormatter.java
+++ src/main/java/org/slog4j/format/TextFormatter.java
@@ -1,159 +1,77 @@
 package org.slog4j.format;
 
-import lombok.experimental.UtilityClass;
 import lombok.val;
 import org.apache.commons.lang3.ClassUtils;
-import org.apache.commons.lang3.text.StrBuilder;
 import org.slf4j.event.Level;
-import org.slog4j.time.TimeProvider;
+import org.slog4j.SLogger;
 
 import java.util.Map;
 
 public class TextFormatter extends BaseFormatter {
-    private static final char PROPERTY_SEP   = ' ';
-    private static final char KV_SEP         = '=';
+    protected static final char PROPERTY_SEP   = ' ';
+    protected static final char NAME_VALUE_SEP = '=';
+
     private static final char OPEN_SUBFIELD  = '[';
     private static final char CLOSE_SUBFIELD = ']';
 
-    public static final TextFormatter INSTANCE = new TextFormatter(true);
+    private static final TextFormatter INSTANCE = new TextFormatter();
 
-    private PropertyFormat timeFormat  = PropertyFormat.PROPERTY;
-    private PropertyFormat levelFormat = PropertyFormat.PROPERTY;
-
-    public enum PropertyFormat {
-        OMIT,
-        VALUE_ONLY,
-        PROPERTY
-    }
-
-    public TextFormatter() {
-    }
-
-    private TextFormatter(boolean immutable) {
-        super(immutable);
-    }
-
-    public TextFormatter timeFormat(PropertyFormat format) {
-        this.timeFormat = format;
-        return this;
-    }
-
-    public TextFormatter levelFormat(PropertyFormat format) {
-        this.levelFormat = format;
-        return this;
-    }
-
-    public TextFormatter commonPropertiesFormat(PropertyFormat format) {
-        return timeFormat(format)
-            .levelFormat(format);
+    public static TextFormatter getInstance() {
+        return INSTANCE;
     }
 
     @Override
-    public String format(TimeProvider timeProvider, Level level, String eventId) {
-        val sb = StrBuilderFactory.get();
-        appendCommonProperties(sb, level, timeProvider);
-        return appendText(sb.appendSeparator(PROPERTY_SEP).append(eventIdLabel()).append(KV_SEP), eventId).toString();
+    public Result format(Level level, long spanId, String eventId) {
+        val sbr = StrBuilderResultFactory.get();
+        beforeAddContentsHook(sbr, level);
+        appendNameValue(sbr, eventIdLabel(), eventId);
+        return appendSpanId(sbr, spanId);
     }
 
     @Override
-    public String format(TimeProvider timeProvider, Level level, String eventId, Object obj) {
-        val sb = StrBuilderFactory.get();
-        appendCommonProperties(sb, level, timeProvider);
-        appendText(sb.appendSeparator(PROPERTY_SEP).append(eventIdLabel()).append(KV_SEP), eventId);
-        return appendComplexObject(sb.append(PROPERTY_SEP), obj).toString();
+    public Result format(Level level, long spanId, String eventId, Object obj) {
+        val sbr = StrBuilderResultFactory.get();
+        beforeAddContentsHook(sbr, level);
+        appendNameValue(sbr, eventIdLabel(), eventId);
+        appendSpanId(sbr, spanId);
+        return appendComplexObject(sbr, obj, true);
     }
 
     @Override
-    public String format(TimeProvider timeProvider, Level level, String eventId, String key, Object value) {
-        val sb = StrBuilderFactory.get();
-        appendCommonProperties(sb, level, timeProvider);
-        appendText(sb.appendSeparator(PROPERTY_SEP).append(eventIdLabel()).append(KV_SEP), eventId);
-        return appendValue(sb.append(PROPERTY_SEP).append(key).append(KV_SEP), value).toString();
+    public Result format(Level level, long spanId, String eventId, String name, Object value) {
+        val sbr = StrBuilderResultFactory.get();
+        beforeAddContentsHook(sbr, level);
+        appendNameValue(sbr, eventIdLabel(), eventId);
+        appendSpanId(sbr, spanId);
+        return appendValue(sbr.appendWithSeparator(PROPERTY_SEP, name).append(NAME_VALUE_SEP), value);
     }
 
     @Override
-    public String format(TimeProvider timeProvider, Level level, String eventId, Object... objs) {
-        val sb = StrBuilderFactory.get();
-        appendCommonProperties(sb, level, timeProvider);
-        appendText(sb.appendSeparator(PROPERTY_SEP).append(eventIdLabel()).append(KV_SEP), eventId);
-        return appendObjects(sb, objs).toString();
+    public Result format(Level level, long spanId, String eventId, Object... objs) {
+        val sbr = StrBuilderResultFactory.get();
+        beforeAddContentsHook(sbr, level);
+        appendNameValue(sbr, eventIdLabel(), eventId);
+        appendSpanId(sbr, spanId);
+        return appendObjects(sbr, objs);
     }
 
-    @Override
-    public String format(TimeProvider timeProvider, Level level, long spanId, String eventId) {
-        val sb = StrBuilderFactory.get();
-        appendCommonProperties(sb, level, timeProvider);
-        return appendText(sb.appendSeparator(PROPERTY_SEP).append(eventIdLabel()).append(KV_SEP), eventId)
-            .append(PROPERTY_SEP)
-            .append(spanIdLabel()).append(KV_SEP)
-            .append(LongIdConverter.convertToString(spanId)).toString();
+    protected StrBuilderResult appendNameValue(StrBuilderResult sbr, String name, String value) {
+        return appendText(sbr.appendWithSeparator(PROPERTY_SEP, name).append(NAME_VALUE_SEP), value);
     }
 
-    @Override
-    public String format(TimeProvider timeProvider, Level level, long spanId, String eventId, Object obj) {
-        val sb = StrBuilderFactory.get();
-        appendCommonProperties(sb, level, timeProvider);
-        appendText(sb.appendSeparator(PROPERTY_SEP).append(eventIdLabel()).append(KV_SEP), eventId)
-            .append(PROPERTY_SEP)
-            .append(spanIdLabel()).append(KV_SEP)
-            .append(LongIdConverter.convertToString(spanId));
-        return appendComplexObject(sb.append(PROPERTY_SEP), obj).toString();
-    }
-
-    @Override
-    public String format(TimeProvider timeProvider, Level level, long spanId, String eventId, String key, Object value) {
-        val sb = StrBuilderFactory.get();
-        appendCommonProperties(sb, level, timeProvider);
-        appendText(sb.appendSeparator(PROPERTY_SEP).append(eventIdLabel()).append(KV_SEP), eventId)
-            .append(PROPERTY_SEP)
-            .append(spanIdLabel()).append(KV_SEP)
-            .append(LongIdConverter.convertToString(spanId));
-        return appendValue(sb.appendSeparator(PROPERTY_SEP).append(key).append(KV_SEP), value).toString();
-    }
-
-    @Override
-    public String format(TimeProvider timeProvider, Level level, long spanId, String eventId, Object... objs) {
-        val sb = StrBuilderFactory.get();
-        appendCommonProperties(sb, level, timeProvider);
-        appendText(sb.appendSeparator(PROPERTY_SEP).append(eventIdLabel()).append(KV_SEP), eventId)
-            .append(PROPERTY_SEP)
-            .append(spanIdLabel()).append(KV_SEP)
-            .append(LongIdConverter.convertToString(spanId));
-        return appendObjects(sb, objs).toString();
-    }
-
-    private void appendCommonProperties(StrBuilder sb, Level level, TimeProvider timeProvider) {
-        switch (timeFormat) {
-            case OMIT:
-                break;
-
-            case VALUE_ONLY:
-                sb.append(FORMAT_ISO8601_MILLIS.format(timeProvider.currentTimeMillis()));
-                break;
-
-            case PROPERTY:
-                sb.append(TIME_LABEL).append(KV_SEP)
-                    .append(FORMAT_ISO8601_MILLIS.format(timeProvider.currentTimeMillis()));
-                break;
+    private StrBuilderResult appendSpanId(StrBuilderResult sbr, long spanId) {
+        if (spanId != SLogger.NO_SPAN_ID) {
+            sbr.appendSeparator(PROPERTY_SEP)
+                .append(spanIdLabel()).append(NAME_VALUE_SEP)
+                .append(LongIdConverter.convertToString(spanId));
         }
-        switch (levelFormat) {
-            case OMIT:
-                break;
-
-            case VALUE_ONLY:
-                sb.appendSeparator(PROPERTY_SEP).append(level.toString());
-                break;
-
-            case PROPERTY:
-                sb.appendSeparator(PROPERTY_SEP).append(LEVEL_LABEL).append(KV_SEP).append(level.toString());
-                break;
-        }
+        return sbr;
     }
 
-    private static StrBuilder appendText(StrBuilder sb, String str) {
+    private static StrBuilderResult appendText(StrBuilderResult sbr, String str) {
         boolean mustQuote = str.indexOf(PROPERTY_SEP) >= 0;
         if (mustQuote) {
-            sb.append('\'');
+            sbr.append('\'');
         }
         char[] chars = str.toCharArray();
         int startIndex = 0;
@@ -169,27 +87,27 @@ public class TextFormatter extends BaseFormatter {
                 charToScape = 'n';
             }
             if (charToScape != Integer.MAX_VALUE) {
-                sb.append(chars, startIndex, index - startIndex)
-                    // TODO: garantir que filtro kv do LogStash considera escaped special chars!
+                sbr.append(chars, startIndex, index - startIndex)
+                    // TODO: check if LogStash kv filter supports escaped chars!
                     .append('\\')
                     .append((char) charToScape);
                 startIndex = index + 1;
             }
         }
         if (startIndex < chars.length) {
-            sb.append(chars, startIndex, str.length() - startIndex);
+            sbr.append(chars, startIndex, str.length() - startIndex);
         }
         if (mustQuote) {
-            sb.append('\'');
+            sbr.append('\'');
         }
-        return sb;
+        return sbr;
     }
 
     @SuppressWarnings({
         "unchecked",
         "squid:ForLoopCounterChangedCheck"  // "for" loop stop conditions should be invariant
     })
-    private StrBuilder appendObjects(StrBuilder sb, Object[] objs) {
+    private StrBuilderResult appendObjects(StrBuilderResult sbr, Object[] objs) {
         for (int i = 0; i < objs.length; i++) {
             Object obj = objs[i];
             if (obj == null) {
@@ -197,87 +115,70 @@ public class TextFormatter extends BaseFormatter {
                 continue;
             }
             if (obj instanceof String) {
-                String key = (String) obj;
-                sb.append(PROPERTY_SEP).append(key).append(KV_SEP);
+                String name = (String) obj;
+                sbr.appendWithSeparator(PROPERTY_SEP, name).append(NAME_VALUE_SEP);
                 if ((i + 1) != objs.length) {
                     Object value = objs[++i];
-                    appendValue(sb, value);
+                    appendValue(sbr, value);
                 } else {
-                    sb.append(MISSING_VALUE_PLACEHOLDER);
+                    sbr.append(MISSING_VALUE_PLACEHOLDER);
                 }
             } else {
-                appendComplexObject(sb.append(PROPERTY_SEP), obj);
+                appendComplexObject(sbr, obj, true);
             }
         }
-        return sb;
+        return sbr;
     }
 
     @SuppressWarnings("squid:S2259")    // Null pointers should not be dereferenced
-    private StrBuilder appendValue(StrBuilder sb, Object obj) {
+    private StrBuilderResult appendValue(StrBuilderResult sbr, Object obj) {
         try {
-            return appendSimpleObject(sb, obj);
+            return appendSimpleObject(sbr, obj);
         } catch (RuntimeException ignored) {
-            return appendComplexObject(sb.append(OPEN_SUBFIELD), obj)
+            return appendComplexObject(sbr.append(OPEN_SUBFIELD), obj, false)
                 .append(CLOSE_SUBFIELD);
         }
     }
 
-    private StrBuilder appendSimpleObject(StrBuilder sb, Object obj) {
+    private StrBuilderResult appendSimpleObject(StrBuilderResult sbr, Object obj) {
         if (obj == null) {
-            return sb.append(NULL_PLACEHOLDER);
+            return sbr.append(NULL_PLACEHOLDER);
         }
         if (obj instanceof String) {
-            return appendText(sb, (String) obj);
+            return appendText(sbr, (String) obj);
         }
         if (ClassUtils.isPrimitiveWrapper(obj.getClass()) || obj.getClass().isEnum()) {
             // don't need to handle special characters here
-            return sb.append(obj.toString());
+            return sbr.append(obj.toString());
         }
         String str = convertToString(obj);
-        return appendText(sb, str);
+        return appendText(sbr, str);
     }
 
     @SuppressWarnings("unchecked")
-    private StrBuilder appendComplexObject(StrBuilder sb, Object obj) {
+    private StrBuilderResult appendComplexObject(StrBuilderResult sbr, Object obj, boolean topLevel) {
         val converter = propertiesConverter(obj.getClass());
+        int loopIndex = topLevel ? 1 : 0;
         if (converter != null) {
-            return appendProperties(sb, converter.convert(obj));
+            return appendProperties(sbr.appendSeparator(PROPERTY_SEP, loopIndex), converter.convert(obj));
         }
-        return sb.append(obj.getClass().getName())
+        if ((obj instanceof Throwable) && (topLevel || (sbr.getAttachment() == null))) {
+            return sbr.setAttachment(obj);
+        }
+        return sbr.appendSeparator(PROPERTY_SEP, loopIndex)
+            .append(obj.getClass().getName())
             .append('#')
             .append(NO_CONVERTER_PLACEHOLDER);
     }
 
-    private StrBuilder appendProperties(StrBuilder sb, Iterable<Map.Entry<?, Object>> props) {
+    private StrBuilderResult appendProperties(StrBuilderResult sbr, Iterable<Map.Entry<?, Object>> props) {
         int loopIndex = 0;
         for (Map.Entry<?, Object> prop : props) {
-            sb.appendSeparator(PROPERTY_SEP, loopIndex++)
+            sbr.appendSeparator(PROPERTY_SEP, loopIndex++)
                 .append(prop.getKey().toString())
-                .append(KV_SEP);
-            appendValue(sb, prop.getValue());
+                .append(NAME_VALUE_SEP);
+            appendValue(sbr, prop.getValue());
         }
-        return sb;
-    }
-
-    @UtilityClass
-    private static class StrBuilderFactory {
-
-        private static final ThreadLocal<StrBuilder> SB_POOL = new ThreadLocal<StrBuilder>() {
-            @Override
-            protected StrBuilder initialValue() {
-                return new StrBuilder();
-            }
-
-            @Override
-            public StrBuilder get() {
-                StrBuilder sb = super.get();
-                sb.clear();
-                return sb;
-            }
-        };
-
-        static StrBuilder get() {
-            return SB_POOL.get();
-        }
+        return sbr;
     }
 }
