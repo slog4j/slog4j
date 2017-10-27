6eb23dfad4967e3f6eb9eac498ab0e74fe59dd97
Eliezio Oliveira <eliezio.oliveira@riocard.com>
2017-10-27 17:25:02 -0200

    Made default Formatter RT configurable via slog4j.yml

--- src/main/java/org/slog4j/SLoggerFactory.java
+++ src/main/java/org/slog4j/SLoggerFactory.java
@@ -4,13 +4,13 @@ import lombok.experimental.UtilityClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slog4j.format.Formatter;
-import org.slog4j.format.TextFormatter;
+import org.slog4j.format.FormatterFactory;
 
 @UtilityClass
 public class SLoggerFactory {
 
     public static SLogger getLogger(String name) {
-        return getLogger(name, TextFormatter.getInstance());
+        return getLogger(name, FormatterFactory.getInstance());
     }
 
     public static SLogger getLogger(String name, Formatter formatter) {
@@ -18,7 +18,7 @@ public class SLoggerFactory {
     }
 
     public static SLogger getLogger(Class<?> clazz) {
-        return getLogger(clazz, TextFormatter.getInstance());
+        return getLogger(clazz, FormatterFactory.getInstance());
     }
 
     public static SLogger getLogger(Class<?> clazz, Formatter formatter) {
