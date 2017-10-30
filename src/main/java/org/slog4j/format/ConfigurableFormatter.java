b9e3db5c1f3ca7dc304d2cfcdd15ce8d7da4979e
Eliezio Oliveira <eliezio@protonmail.com>
2017-10-30 17:01:16 -0200

    Made 'time' and 'level' property labels configurable

--- src/main/java/org/slog4j/format/ConfigurableFormatter.java
+++ src/main/java/org/slog4j/format/ConfigurableFormatter.java
@@ -8,8 +8,24 @@ import org.joda.convert.ToStringConverter;
 public interface ConfigurableFormatter extends Formatter {
 
     /**
-     * Changes the label used for the mandatory <code>event</code> property. To avoid any implicit transformation, the
-     * provided label cannot contain any space and/or characters that would require quoting (i.e: '\'', '[', ']').
+     * Changes the label used to the <code>time</code> property.
+     *
+     * @param timeLabel The new label.
+     * @return this, to enable chaining.
+     */
+    Formatter timeLabel(String timeLabel);
+
+    /**
+     * Changes the label used to the <code>level</code> property.
+     *
+     * @param levelLabel The new label.
+     * @return this, to enable chaining.
+     */
+    Formatter levelLabel(String levelLabel);
+
+    /**
+     * Changes the label used for the mandatory <code>eventId</code> property. To avoid any implicit transformation, the
+     * provided label MUST not contain any space and/or characters that would require quoting (i.e: '\'', '[', ']').
      *
      * @param eventIdLabel The new label.
      * @return this, to enable chaining.
